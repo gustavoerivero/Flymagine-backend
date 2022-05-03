e862bf0b7d8503a23386f46a92c6b8534428f949
Gustavo Rivero <gustavoerivero12@gmail.com>
2022-05-02 21:34:29 -0400

    Update User Controller

--- controllers/CUser.js
+++ controllers/CUser.js
@@ -8,31 +8,33 @@ const validate = require('../utils/validate')
 const createUser = async (req, res) => {
   try {
     const value = req.body
-    
+
     const _User = await mUser.findOne({
       email: value.email
     })
 
     if (_User) {
       resp.makeResponsesError(res, "UFound")
-    }
+    } else {
 
-    const user = new mUser({
-      idRole: value.idRole,
-      firstName: value.firstName,
-      lastName: value.lastName,
-      email: value.email,
-      password: bcrypt.hashSync(value.password),
-      photo: value.photo,
-      address: value.address,
-      phone: value.phone,
-      biography: value.biography,
-      birthday: value.birthday
-    })
-    
-    const saveUser = await user.save()
-    console.log(saveUser)
-    resp.makeResponsesOkData(res, saveUser, "UCreated")
+      const user = new mUser({
+        idRole: value.idRole,
+        firstName: value.firstName,
+        lastName: value.lastName,
+        email: value.email,
+        password: bcrypt.hashSync(value.password),
+        photo: value.photo,
+        address: value.address,
+        phone: value.phone,
+        biography: value.biography,
+        birthday: value.birthday
+      })
+
+      const saveUser = await user.save()
+      console.log(saveUser)
+      resp.makeResponsesOkData(res, saveUser, "UCreated")
+
+    }
 
   } catch (error) {
     resp.makeResponsesException(res, error)
@@ -66,16 +68,16 @@ const login = async (req, res) => {
       }
     )
 
-    const user = {      
+    const user = {
       idRole: valUser.idRole,
       firstName: valUser.name,
-      lastName: valUser.lastName,      
+      lastName: valUser.lastName,
       email: valUser.email,
       photo: valUser.photo,
       address: valUser.address,
       phone: valUser.phone,
-      birthday: valUser.birthday,  
-      biography: valUser.biography,    
+      birthday: valUser.birthday,
+      biography: valUser.biography,
       token: token
     }
 
@@ -97,7 +99,7 @@ const getAllUsers = async (req, res) => {
 
 const getUser = async (req, res) => {
   try {
-    const user = await mUser.findOne({_id: req.params.id})
+    const user = await mUser.findOne({ _id: req.params.id })
     resp.makeResponsesOkData(res, user, "Success")
   } catch (error) {
     resp.makeResponsesError(res, error)
@@ -106,20 +108,18 @@ const getUser = async (req, res) => {
 
 const updateUser = async (req, res) => {
   try {
-    const user = await mUser.findOne({_id: req.params.id})
+    const user = await mUser.findOne({ _id: req.params.id })
     const data = req.body
 
-    if(!user) {
+    if (!user) {
       return resp.makeResponsesError(res, "UNotFound")
+    } else {
+
+      const saveUser = await mUser.findByIdAndUpdate(req.params.id, data)
+
+      resp.makeResponsesOkData(res, saveUser, "UUpdated")
+
     }
-
-    const saveUser = await user.updateOne({
-      _id: req.params.id,
-    }, {
-      $set: data
-    })
-
-    resp.makeResponsesOkData(res, updateUser, "UUpdated")
 
   } catch (error) {
     resp.makeResponsesError(res, error)
@@ -128,9 +128,9 @@ const updateUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {
   try {
-    const user = await mUser.findOne({_id: req.params.id})
+    const user = await mUser.findOne({ _id: req.params.id })
 
-    if(!user) {
+    if (!user) {
       return resp.makeResponsesError(res, "UNotFound")
     }
 
