2d9b93ca824679f69cd97ce99a9c04e88855c934
Gustavo Rivero <gustavoerivero12@gmail.com>
2022-05-17 23:21:47 -0400

    Update

--- routes/RPost.js
+++ routes/RPost.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 const cPost = require('../controllers/CPost')
+const { imageUpload } = require('../utils/multer')
 
 router.post('/', cPost.createPost)
 router.get('/', cPost.getAllPosts)
 router.get('/:id', cPost.getPostById)
 router.get('/user/:id', cPost.getPostByUser)
 router.post('/feed', cPost.getFeedPosts)
-router.post('/:id/image', cPost.uploadImage)
+router.post('/:id/image', imageUpload.single('photo'), cPost.uploadImage)
 router.put('/:id', cPost.updatePost)
 router.delete('/:id', cPost.deletePost)
 
